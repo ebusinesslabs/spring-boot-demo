5e1742c6f0a98f3563e1355b4b2d5cf8cec0891c
Δημήτρης <dimitris.vossos@gmail.com>
2018-03-27 16:55:01 +0300

    dashboard template

--- src/main/java/com/example/sbdemo/admin/DashboardController.java
+++ src/main/java/com/example/sbdemo/admin/DashboardController.java
@@ -1,17 +1,19 @@
 package com.example.sbdemo.admin;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Controller
 public class DashboardController {
 
     private static final Map<String,String> TEMPLATE;
     static {
         TEMPLATE  = new HashMap<>();
-        TEMPLATE.put("VIEW", "home");
+        TEMPLATE.put("VIEW", "dashboard");
         TEMPLATE.put("TITLE", "Dashboard");
     }
 
