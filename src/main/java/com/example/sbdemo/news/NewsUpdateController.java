76624ff3216b382588a7b62430ccd0941e36ecc6
Δημήτρης (iLefkippos) <dimitris.vossos@gmail.com>
2018-04-16 09:16:19 +0300

    upload file with news

--- src/main/java/com/example/sbdemo/news/NewsUpdateController.java
+++ src/main/java/com/example/sbdemo/news/NewsUpdateController.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -19,6 +21,7 @@ import java.util.Optional;
 @Controller
 public class NewsUpdateController {
 
+    private static final String UPLOAD_DIR = "src/main/resources/static/images/";
     private NewsRepository newsRepository;
 
     @Autowired
@@ -38,7 +41,21 @@ public class NewsUpdateController {
     }
 
     @PostMapping(value = "/news/{id:[\\d]+}")
-    public String updateNews(@ModelAttribute News news, RedirectAttributes redirectAttributes) {
+    public String updateNews(@ModelAttribute News news,
+                             RedirectAttributes redirectAttributes,
+                             @RequestParam("pic_file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOAD_DIR + file.getOriginalFilename());
+                Files.write(path, bytes);
+                news.setPicture(file.getOriginalFilename());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "views/news-update";
+            }
+        }
+
         this.newsRepository.save(news);
         redirectAttributes.addFlashAttribute("message", "Record saved successfully.");
         return "redirect:/news";
