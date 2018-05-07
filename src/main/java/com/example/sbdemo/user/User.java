cb35c9e2af212d9d743a0016c52efa9894eeaa73
Ebusiness Lab <ebusinesslabs@gmail.com>
2018-05-07 09:16:56 +0300

    προσπάθεια για δυναμική ενημέρωση δεδομένων

--- src/main/java/com/example/sbdemo/user/User.java
+++ src/main/java/com/example/sbdemo/user/User.java
@@ -1,34 +1,35 @@
 package com.example.sbdemo.user;
 
+import org.hibernate.annotations.DynamicUpdate;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
+@DynamicUpdate
 @Table(name="users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty
     @Size(min = 3, max = 30)
     private String fname;
 
-    @NotEmpty
     @Size(min = 3, max = 30)
     private String lname;
 
-    @NotEmpty
     @Size(min = 6, max = 60)
     @Email
     private String email;
 
-    @NotEmpty
-    @Size(min = 6)
     private String password;
 
+    @Transient
+    private String confirmPassword;
 
     private String roles;
 
@@ -79,5 +80,18 @@ public class User {
 
     public void setRoles(String roles) {
         this.roles = roles;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d, %s, %s, %s, %s, %s", id, fname, lname, email, password, roles);
     }
 }
--- src/main/java/com/example/sbdemo/user/UserController.java
+++ src/main/java/com/example/sbdemo/user/UserController.java
@@ -1,16 +1,21 @@
 package com.example.sbdemo.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
 public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -19,7 +24,7 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/user")
+    @GetMapping("/users")
     public String showListUsers(Model model) {
         model.addAttribute("users", this.userRepository.findAll());
         return "views/user-list";
@@ -33,5 +38,20 @@ public class UserController {
         }
         model.addAttribute("user", optionalUser.get());
         return "views/user-update";
+    }
+
+    @PostMapping(value = "/user")
+    public String updateUser(@ModelAttribute User user, BindingResult bindingResult) {
+        logger.info(bindingResult.getModel().toString());
+
+        if (bindingResult.hasErrors()) {
+            return "views/user-update";
+        }
+//        User dbUser  = userRepository.findById(user.getId()).get();
+//        dbUser.setFname(user.getFname());
+//        dbUser.setLname(user.getLname());
+//        dbUser.setEmail(user.getEmail());
+        userRepository.save(user);
+        return "redirect:/users";
     }
 }
